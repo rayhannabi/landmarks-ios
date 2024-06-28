7a4344c7903f75bd97e152cb14dd097c16456500
Rayhan Nabi <rayhanjanam@gmail.com>
2024-06-28 15:44:25 +0600

    initial commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Md. Rayhan Nabi on 28/6/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
